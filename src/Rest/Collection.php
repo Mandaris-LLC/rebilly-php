03c40d5693771ceda87a319d77daf7551375b8f3
ae94 <ae94@icloud.com>
2018-08-16 16:51:38 +0200

    Update Collection.php
    added exception for payments

--- src/Rest/Collection.php
+++ src/Rest/Collection.php
@@ -72,7 +72,7 @@ class Collection implements JsonSerializable, IteratorAggregate, ArrayAccess, Co
             unset($data['_metadata']);
         }
 
-        if (isset($data['data'])) {
+        if (isset($data['data']) && !((new \ReflectionClass($this->prototype))->getShortName() == 'Payment')) {
             $data = $data['data'];
         }
 
